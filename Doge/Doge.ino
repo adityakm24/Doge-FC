7e9addd0f4ee380bfeb9e47dd6409edacafa23b6
Aditya Krishnan Mohan <38864621+adityakm24@users.noreply.github.com>
2022-02-16 13:06:51 +0530

    Added files via upload

--- Doge/Doge.ino
+++ Doge/Doge.ino
@@ -1,36 +1,138 @@
-#include <SPI.h>
-#include <SD.h>
-File myFile;
+#include <SD.h> //SD Card
+//bmp280
+#include <Wire.h>
+#include "SPI.h" //Why? Because library supports SPI and I2C connection
+#include <Adafruit_Sensor.h>
+#include "Adafruit_BMP280.h"
+
+//MPU
+#include "I2Cdev.h"
+#include "I2Cdev.h"
+#include "MPU6050_6Axis_MotionApps20.h"
+//#include "MPU6050.h" // not necessary if using MotionApps include file
+
+
+//bmp initialization
+// define device I2C address: 0x76 or 0x77 (0x77 is library default address)
+#define BMP280_I2C_ADDRESS  0x76
+Adafruit_BMP280 bmp280;
+
+const int chipSelect = 4; //SD Card Pin
+
 void setup() {
-// Open serial communications and wait for port to open:
-Serial.begin(9600);
-while (!Serial) {
-; // wait for serial port to connect. Needed for native USB port only
+  Serial.begin(9600);
+  Serial.println(F("Arduino + BMP280"));
+  
+  if (!bmp280.begin(BMP280_I2C_ADDRESS))
+  {  
+    Serial.println("Could not find a valid BMP280 sensor, check wiring!");
+    while (1);
+  }
+ //mpu code
+  Wire.begin();
+  Wire.beginTransmission(MPU6050_addr);
+  Wire.write(0x6B);
+  Wire.write(0);
+  Wire.endTransmission(true);
+  
+
+  //SD Card init
+  Serial.print("Initializing SD card...");
+
+  // see if the card is present and can be initialized:
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Card failed, or not present");
+    // don't do anything more:
+    while (1);
+  }
+  Serial.println("card initialized.");
 }
-Serial.print("Initializing SD card...");
-if (!SD.begin(10)) {
-Serial.println("initialization failed!");
-while (1);
+
+
+char text[14]; 
+ 
+
+
+
+// main loop
+void loop()
+{
+  // get temperature, pressure and altitude from library
+  float temperature = bmp280.readTemperature();  // get temperature
+  float pressure    = bmp280.readPressure();     // get pressure
+  float altitude_   = bmp280.readAltitude(1013.25); // get altitude (this should be adjusted to your local forecast)
+ 
+  // print data on the serial monitor software(bmp280)
+  // 1: print temperature
+  Serial.print("Temperature = ");
+  Serial.print(temperature);
+  Serial.println(" °C");
+  // 2: print pressure
+  Serial.print("Pressure    = ");
+  Serial.print(pressure/100);
+  Serial.println(" hPa");
+  // 3: print altitude
+  Serial.print("Approx Altitude = ");
+  Serial.print(altitude_);
+  Serial.println(" m");
+    
+  Serial.println();  // start a new line
+  //delay(1000);   uncomment this if you need
+
+
+
+
+  // print data on to the SD Card(bmp280)
+  // 1: print temperature
+  dataFile.print("Temperature = ");
+  dataFile.print(temperature);
+  dataFile.println(" °C");
+  // 2: print pressure
+  dataFile.print("Pressure    = ");
+  dataFile.print(pressure/100);
+  dataFile.println(" hPa");
+  // 3: print altitude
+  dataFile.print("Approx Altitude = ");
+  dataFile.print(altitude_);
+  dataFile.println(" m");
+    
+  dataFile.println();  // start a new line
+  //delay(1000);   uncomment this if you need
+
+
+
+
+
+  //mpu code
+  Wire.beginTransmission(MPU6050_addr);
+  Wire.write(0x3B);
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU6050_addr,14,true);
+  AccX=Wire.read()<<8|Wire.read();
+  AccY=Wire.read()<<8|Wire.read();
+  AccZ=Wire.read()<<8|Wire.read();
+  Temp=Wire.read()<<8|Wire.read();
+  GyroX=Wire.read()<<8|Wire.read();
+  GyroY=Wire.read()<<8|Wire.read();
+  GyroZ=Wire.read()<<8|Wire.read();
+  //serial printing MPU values
+  Serial.print("AccX = "); Serial.print(AccX);
+  Serial.print(" || AccY = "); Serial.print(AccY);
+  Serial.print(" || AccZ = "); Serial.print(AccZ);
+  Serial.print(" || Temp = "); Serial.print(Temp/340.00+36.53);
+  Serial.print(" || GyroX = "); Serial.print(GyroX);
+  Serial.print(" || GyroY = "); Serial.print(GyroY);
+  Serial.print(" || GyroZ = "); Serial.println(GyroZ);
+//serial printing MPU values
+  dataFile.print("AccX = "); Serial.print(AccX);
+  dataFile.print(" || AccY = "); Serial.print(AccY);
+  dataFile.print(" || AccZ = "); Serial.print(AccZ);
+  dataFile.print(" || Temp = "); Serial.print(Temp/340.00+36.53);
+  dataFile.print(" || GyroX = "); Serial.print(GyroX);
+  dataFile.print(" || GyroY = "); Serial.print(GyroY);
+  dataFile.print(" || GyroZ = "); Serial.println(GyroZ);
+  
 }
-Serial.println("initialization done.");
-// open the file. note that only one file can be open at a time,
-// so you have to close this one before opening another.
-myFile = SD.open("test.txt", FILE_WRITE);
-// if the file opened okay, write to it:
-if (myFile) {
-Serial.print("Writing to test.txt...");
-myFile.println("This is a test file :)");
-myFile.println("testing 1, 2, 3.");
-for (int i = 0; i < 20; i++) {
-myFile.println(i);
-}
-// close the file:
-myFile.close();
-Serial.println("done.");
-} else {
-// if the file didn't open, print an error:
-Serial.println("error opening test.txt");
-}
-}
-void loop() {
-// nothing happens after setup
+// end of code.
+ 
+ 
